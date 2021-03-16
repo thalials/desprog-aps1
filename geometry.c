e3b0442dad1166c325233fe1b65bb39491b895e4
TheoBR00 <theobr17@gmail.com>
2021-03-16 14:47:37 -0300

    funcao verify

--- geometry.c
+++ geometry.c
@@ -2,13 +2,19 @@
 
 #include "geometry.h"
 
-typedef struct{
-    int x;
-    int y;
-} point;
+//typedef struct{
+ //   int x;
+ //   int y;
+//}point;
 
 int verify(point p, point a, point b) {
-    
+    double tam_aresta = pow((pow(a.x - b.x, 2) + pow(a.y - b.y, 2)), 0.5);
+    double tam_a = pow((pow(a.x - p.x, 2) + pow(a.y - p.y, 2)), 0.5);
+    double tam_b = pow((pow(b.x - p.x, 2) + pow(b.y - p.y, 2)), 0.5);
+
+    if(abs(tam_a + tam_b - tam_aresta) < 0.000001){
+        return 2;
+    }
 
     return 0;
 }
@@ -16,3 +22,4 @@ int verify(point p, point a, point b) {
 int inside(point p, point poly[], int n) {
     return 0;
 }
+
